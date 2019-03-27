9e349e2d199a203f0dc429a6c52451955509e038
Andrew French <afr66@uclive.ac.nz>
2019-03-27 17:31:52 +1300

    Added in a the Neo4j driver to allow for simple calls to a local neo4j instance

--- backend-api/backend-api/Controllers/RandomController.cs
+++ backend-api/backend-api/Controllers/RandomController.cs
@@ -6,6 +6,7 @@ using System.Web.Http.Cors;
 using backend_api.Models;
 
 using Microsoft.AspNetCore.Mvc;
+using Neo4j.Driver.V1;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -31,7 +32,32 @@ namespace backend_api.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+
+            using (var driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "andy")))
+            {
+                using (var session = driver.Session())
+                {
+                    //session.Run("CREATE (a:Person {name:'Andy', title:'SoftwareDev'})");
+                    //session.Run("CREATE (a:Person {name:'Buzz', title:'The II'})");
+                    ////var result = session.Run("MATCH (a:Person) WHERE a.name = 'Arthur' RETURN a.name AS name, a.title AS title");
+
+                    ////foreach (var record in result)
+                    ////Console.WriteLine($"{record["title"].As<string>()} {record["name"].As<string>()}");
+                    Console.WriteLine("----------------------");
+
+                    var greeting = session.WriteTransaction(tx =>
+                    {
+                        var result = tx.Run("CREATE (Andy:Person {name:'Andy', title:'SoftwareDev'})");
+                        return result;
+                    });
+                    Console.WriteLine(greeting);
+                    Console.WriteLine("----------------------");
+                }
+
+            }
+
+
+            return "Yay!";
         }
 
         // POST api/values
