082fad388a3178c591b1189cd9f1f6e59f06deed
Andrew French <afr66@uclive.ac.nz>
2019-04-02 20:52:39 +1300

    Created a new modal to display

--- frontend-web/src/Project/Project.js
+++ frontend-web/src/Project/Project.js
@@ -9,28 +9,32 @@ import {
   List,
   Menu,
   Segment,
+  Button,
 } from 'semantic-ui-react'
 
 import TopNavBar from '../Common/TopNavBar'
 
 import SteveImage from '../Images/steve.jpg'
+import CreateProjectModal from './CreateProjectModal';
 
-const FixedMenuLayout = () => (
-  <div>
+class ProjectPageLayout extends React.Component {
 
+  render() {
+    return (
+      <div>
         <TopNavBar/>
+
+        <Button style={{ marginTop: '5em' }} onClick={CreateProjectModal}>Create Project</Button>
 
         <Grid divided='vertically' style={{ marginTop: '5em' }} centered>
         <Grid.Row columns={2}>
         <Grid.Column width={2}>
             <Image style={{'border-radius':8}} src={SteveImage} size='small' />    
         </Grid.Column>
-        <Grid.Column centered >
+        <Grid.Column centered>
         <Header as='h1'>Project Title</Header>
-            <p>This is a basic fixed menu template using fixed size containers.</p>
-            <p>
-                A text container is used for the main container, which is useful for single column layouts.
-            </p>
+            <p>Description</p>
+            A text container is used for the main container, which is useful for single column layouts.
         </Grid.Column>
         </Grid.Row>
 
@@ -45,78 +49,78 @@ const FixedMenuLayout = () => (
             <Image src='/images/wireframe/paragraph.png' />
         </Grid.Column>
         </Grid.Row> */}
-    </Grid>
-
-    <Container text >
-
-
-      
-
-    </Container>
-
-
-
-
-
-
-
-    <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
-      <Container textAlign='center'>
-        <Grid divided inverted stackable>
-          <Grid.Column width={3}>
-            <Header inverted as='h4' content='Group 1' />
-            <List link inverted>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-              <List.Item as='a'>Link Three</List.Item>
-              <List.Item as='a'>Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as='h4' content='Group 2' />
-            <List link inverted>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-              <List.Item as='a'>Link Three</List.Item>
-              <List.Item as='a'>Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as='h4' content='Group 3' />
-            <List link inverted>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-              <List.Item as='a'>Link Three</List.Item>
-              <List.Item as='a'>Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={7}>
-            <Header inverted as='h4' content='Footer Header' />
-            <p>
-              Extra space for a call to action inside the footer that could help re-engage users.
-            </p>
-          </Grid.Column>
         </Grid>
 
-        <Divider inverted section />
-        <Image centered size='mini' src='/logo.png' />
-        <List horizontal inverted divided link size='small'>
-          <List.Item as='a' href='#'>
-            Site Map
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Contact Us
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Terms and Conditions
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Privacy Policy
-          </List.Item>
-        </List>
-      </Container>
-    </Segment>
-  </div>
-)
+        <Container text >
+        
+        </Container>
 
-export default FixedMenuLayout
+
+
+
+
+
+
+        <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+          <Container textAlign='center'>
+            <Grid divided inverted stackable>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 1' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 2' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as='h4' content='Group 3' />
+                <List link inverted>
+                  <List.Item as='a'>Link One</List.Item>
+                  <List.Item as='a'>Link Two</List.Item>
+                  <List.Item as='a'>Link Three</List.Item>
+                  <List.Item as='a'>Link Four</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={7}>
+                <Header inverted as='h4' content='Footer Header' />
+                <p>
+                  Extra space for a call to action inside the footer that could help re-engage users.
+                </p>
+              </Grid.Column>
+            </Grid>
+
+            <Divider inverted section />
+            <Image centered size='mini' src='/logo.png' />
+            <List horizontal inverted divided link size='small'>
+              <List.Item as='a' href='#'>
+                Site Map
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Contact Us
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Terms and Conditions
+              </List.Item>
+              <List.Item as='a' href='#'>
+                Privacy Policy
+              </List.Item>
+            </List>
+          </Container>
+        </Segment>
+      </div>
+
+    );
+  }
+}
+
+export default ProjectPageLayout
