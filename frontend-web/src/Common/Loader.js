f02ffd8f2a2f68927bf4e591db9c9dc559e5649a
Matthew Knight <knightpmatthew@gmail.com>
2019-05-01 01:14:34 +1200

    -Corrected Redux flow to fetch a profile -Formed a loader and now displaying profile data

---
+++ frontend-web/src/Common/Loader.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Loader } from 'semantic-ui-react'
+
+const LoaderInlineCentered = () => <Loader active inline='centered' />;
+
+export default LoaderInlineCentered
