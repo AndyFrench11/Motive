0d3afaf9da34b4bc0bbe5af7b4e56e67dc12992f
Matthew Knight <knightpmatthew@gmail.com>
2019-09-23 17:55:45 +1200

    Created thread on timer to clear inactive sessions

--- backend-api/backend-api/Controllers/SessionsController.cs
+++ backend-api/backend-api/Controllers/SessionsController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using System.Threading;
 using backend_api.Crypto;
 using backend_api.Models;
 
@@ -9,10 +10,35 @@ namespace backend_api.Controllers
     public class SessionsController
     {
         private static readonly Dictionary<string, Session> LoggedInSessions;
+        private static readonly Timer sessionGcTimer;
 
         static SessionsController()
         {
             LoggedInSessions = new Dictionary<string, Session>();
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(1);
+
+            sessionGcTimer = new Timer((e) =>
+            {
+                ClearOldSessions();   
+            }, null, startTimeSpan, periodTimeSpan);
+        }
+
+        private static void ClearOldSessions()
+        {
+            Console.WriteLine("Clearing inactive sessions...");
+            int numOfClosedSessions = 0;
+            foreach(KeyValuePair<string, Session> sessionEntry in LoggedInSessions)
+            {
+                if (sessionEntry.Value.expiry < DateTime.Now)
+                {
+                    // Session has expired -> remove its entry
+                    CloseSession(sessionEntry.Key);
+                    numOfClosedSessions++;
+                }
+            }
+            Console.WriteLine($"Closed {numOfClosedSessions} sessions.");
         }
         
         public static string CreateSession(Guid userGuid, Session newSession)
