6a897688686811f33bc417f62e3e0a332afd0848
Matilda Porterfield <mpo74@uclive.ac.nz>
2019-10-22 18:54:08 +1300

    Not found and forbidden for channels.

--- backend-api/backend-api/Features/TaskForum/Repository/IChannelRepository.cs
+++ backend-api/backend-api/Features/TaskForum/Repository/IChannelRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using backend_api.Database;
 using backend_api.Features.TaskForum.Models;
+using backend_api.Models;
 
 namespace backend_api.Features.TaskForum.Repository
 {
@@ -14,5 +15,9 @@ namespace backend_api.Features.TaskForum.Repository
         RepositoryReturn<bool> Edit(Channel channel);
         
         RepositoryReturn<bool> Delete(Guid channelGuid);
+        
+        RepositoryReturn<bool> Exists(Guid channelGuid);
+        
+        RepositoryReturn<ProjectTask> GetTask(Guid channelGuid);
     }
 }
